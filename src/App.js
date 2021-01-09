5535a1eafc158901ac79a81e06c06f0aafd8d928
landy <landykingdom@gmail.com>
2021-01-09 23:21:59 +0800

    update layout.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  CssBaseline,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import "./App.css";
+
+function GameItem({ image, imageTitle, title, description }) {
+  return (
+    <Card elevation={3}>
+      <CardActionArea>
+        <CardMedia
+          // className={classes.media}
+          image={image}
+          title={imageTitle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
 
 function App() {
+  const games = [
+    {
+      id: "tic-tac-toe",
+      title: "Tic-Tac-Toe",
+      imageTitle: "Tic-Tac-Toe",
+      description: "A Tic-Tac-Toe game with AI written by React JS.",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" style={{ height: "100vh" }}>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          spacing={4}
+          style={{ height: "100%" }}
+          direction="column"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {games.map((e) => (
+            <Grid item key={e.id}>
+              <GameItem {...e} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
