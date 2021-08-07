4e06ec313e9bc9673dbaccabd460f1acb22a9b2e
landyking <landykingdom@gmail.com>
2021-08-07 15:29:50 +0800

    make the home module depend on the tic-tac-toe module

--- packages/home/src/App.js
+++ packages/home/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TicTacToe from "tic-tac-toe";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <TicTacToe />
       </header>
     </div>
   );
