55690adcfb2998b4ecf4539cc18a8be371a15404
landyking <landykingdom@gmail.com>
2021-08-07 20:54:58 +0800

    lazy load modules.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Notes
+
+- must use `lerna clean && lerna bootstrap` to install dependencies. Lerna will create link automatically.
--- packages/home/src/index.js
+++ packages/home/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./index.css";
+
+const menus = [
+  {
+    path: "/tic-tac-toe",
+    title: "Tic Tac Toe",
+    cmp: React.lazy(() => import("tic-tac-toe")),
+  },
+];
+
+function Home() {
+  return (
+    <div className="center full-height">
+      <div className="home">
+        <h2>Component List</h2>
+        <nav>
+          <ul>
+            {menus.map((e) => (
+              <li key={e.path}>
+                <Link to={e.path}>{e.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <React.Suspense fallback={<div className="center">loading...</div>}>
+        <Switch>
+          {menus.map((e) => {
+            const CMP = e.cmp;
+            return (
+              <Route key={e.path} path={e.path}>
+                <CMP />
+              </Route>
+            );
+          })}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </React.Suspense>
+    </Router>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
