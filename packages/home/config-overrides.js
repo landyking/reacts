78544d7a90dbfa041c88da1ff97aab63ab09950e
landyking <landykingdom@gmail.com>
2021-08-07 21:41:10 +0800

    add comments

--- packages/home/config-overrides.js
+++ packages/home/config-overrides.js
@@ -8,6 +8,7 @@ const { override, addWebpackAlias } = require("customize-cra");
 
 module.exports = override(
   addWebpackAlias({
+    // Use alias to resolve the duplicate React instances problem. See here https://github.com/facebook/react/issues/13991#issuecomment-435587809
     react: resolve("node_modules/react"),
     "react-dom": resolve("node_modules/react-dom"),
   })
